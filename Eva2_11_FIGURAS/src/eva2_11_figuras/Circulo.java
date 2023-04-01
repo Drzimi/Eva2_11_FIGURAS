e0ccf58da12e95c6d848a4e1a31c344132dc1990
Drzimi <124393239+Drzimi@users.noreply.github.com>
2023-03-31 21:56:11 -0600

    Add files via upload

---
+++ Eva2_11_FIGURAS/src/eva2_11_figuras/Circulo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_11_figuras;
+
+/**
+ *
+ * @author invitado
+ */
+public class Circulo extends Figuras{
+    private double radio;
+  
+  class radio(){
+   
+      return;
+  }   
+    
+}
---
+++ Eva2_11_FIGURAS/src/eva2_11_figuras/Figuras.java
@@ -0,0 +1,17 @@
+
+
+package eva2_11_figuras;
+
+
+public class Figuras {
+   public double calcularArea(){
+       return -1;
+   }
+    public double calcularPeri(){
+       return -1;
+   }
+ 
+
+}
+
+
